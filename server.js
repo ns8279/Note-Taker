d226b5acdca5d361be52e949642353d52fe89f57
Nirupama Sundararaman <nirupama.2308@gmail.com>
2020-05-23 18:30:18 -0700

    trying to fix the POST method

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /*
     Dependcies
+    ===============================================================================
 */
 const express = require('express');
 const path = require('path');
@@ -7,16 +8,66 @@ const fs = require('fs');
 
 /* 
     Intialize express
+    =================================================================================
 */
 const app = express();
-const PORT = process.env.PORT || 3003 //This is necessary for deploying it in heroku 
+const PORT = process.env.PORT || 3001 //This is necessary for deploying it in heroku 
 
 /* 
     Middleware functions
+    ===================================================================================
 */
 
-app.use(express.urlencoded({ extended:true }));
-app.use(express.static('public'));
-app.use(express.json);
+// parse incoming string or array data
+ app.use(express.urlencoded ( { extended: true }));
+// // parse incoming JSON data
+ app.use(express.json());
+// // middleware for public files
+ app.use(express.static(path.join(__dirname, "/public")));
 
+/*
+    request data 
+    ===================================================================================== 
+*/ 
+const { notes } = require('./db/db.json');
+
+
+/* 
+    ROUTES
+    =====================================================================================
+*/
+
+//GET 
+
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"))
+});
+
+app.get('/' , (req, res)=> {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get('/api/notes', (req, res) => {
+    //return res.sendFile(path.join(__dirname, '/db/', 'db.json'));
+    return  res.json(notes); 
+});
+
+//POST 
+app.post('api/notes', (req,res) => {
+    const newNote = req.body;     
+    console.log(newNote);     
+    notes.push(newNote);    
+    res.json(newNote);
+      
+});
+
+
+/*
+    Listner
+    ======================================================================================
+    This code will start the server
+*/ 
+app.listen(PORT, () => {
+    console.log(`App is now listening on ${PORT}`);
+});
 
